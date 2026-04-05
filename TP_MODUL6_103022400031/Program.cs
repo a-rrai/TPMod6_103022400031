77bfd74b8deb86689665f7bd84bdc6dc546ff77b
Raissha Najwa Maharani <raissha.njw@gmail.com>
2026-04-05 14:20:14 +0700

    Menambahlan class SayaMusicTrack

--- TP_MODUL6_103022400031/Program.cs
+++ TP_MODUL6_103022400031/Program.cs
@@ -1,2 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace TP_MODUL6_NIM
+{
+    class SayaMusicTrack
+    {
+        private int id;
+        private int playCount;
+        private string title;
+
+        public SayaMusicTrack(string title)
+        {
+            this.title = title;
+
+            Random rnd = new Random();
+            this.id = rnd.Next(10000, 100000);
+
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine($"Track ID   : {this.id}");
+            Console.WriteLine($"Title      : {this.title}");
+            Console.WriteLine($"Play Count : {this.playCount}");
+            Console.WriteLine("---------------------------------");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaMusicTrack track1 = new SayaMusicTrack("RUDE! - Hearts2Hearts");
+
+            Console.WriteLine("Kondisi Awal:");
+            track1.PrintTrackDetails();
+
+            Console.WriteLine("Setelah ditambah play count:");
+            track1.IncreasePlayCount(500);
+            track1.PrintTrackDetails();
+        }
+    }
+}
--- TP_MODUL6_103022400031/obj/Debug/net10.0/TP_MODUL6_103022400031.AssemblyInfo.cs
+++ TP_MODUL6_103022400031/obj/Debug/net10.0/TP_MODUL6_103022400031.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL6_103022400031")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a27e86e6322f97640b4cf3fef8484cbd9948354a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL6_103022400031")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL6_103022400031")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
